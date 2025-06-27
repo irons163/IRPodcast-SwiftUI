a4d196ba76f070c96c85208a32b641e3537a6d7e
Phil <irons7654321@gmail.com>
2025-06-27 20:49:21 +0800

    Initial Commit

---
+++ IRPodcast-SwiftUI/IRPodcast_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IRPodcast_SwiftUIApp.swift
+//  IRPodcast-SwiftUI
+//
+//  Created by Phil on 2025/6/27.
+//
+
+import SwiftUI
+
+@main
+struct IRPodcast_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IRPodcast-SwiftUITests/IRPodcast_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  IRPodcast_SwiftUITests.swift
+//  IRPodcast-SwiftUITests
+//
+//  Created by Phil on 2025/6/27.
+//
+
+import Testing
+@testable import IRPodcast_SwiftUI
+
+struct IRPodcast_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
