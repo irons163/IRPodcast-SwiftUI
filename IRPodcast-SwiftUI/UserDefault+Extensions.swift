6842ee22094818970cd15651afffcd5a8ce0d6d9
Phil <irons7654321@gmail.com>
2025-06-30 01:02:11 +0800

    Extract FavoritesManager

--- IRPodcast-SwiftUI/UserDefault+Extensions.swift
+++ IRPodcast-SwiftUI/UserDefault+Extensions.swift
@@ -9,49 +9,6 @@ import Foundation
 import SwiftUI
 import Combine
 
-protocol FavoritesManagerProtocol: ObservableObject {
-    
-    var favoritedPodcasts: [Podcast] { get }
-    func add(_ podcast: Podcast)
-    func remove(_ podcast: Podcast)
-    init()
-}
-
-class MockFavoritesManager: FavoritesManagerProtocol {
-    
-    var favoritedPodcasts: [Podcast] = []
-    
-    required init() { }
-
-    func add(_ podcast: Podcast) {
-        favoritedPodcasts.append(podcast)
-    }
-    
-    func remove(_ podcast: Podcast) {
-        favoritedPodcasts.removeAll(where: { $0 == podcast })
-    }
-}
-
-class FavoritesManager: FavoritesManagerProtocol {
-    @Published var favoritedPodcasts: [Podcast] = []
-    
-    private let store = FavoritesDataStore()
-    
-    required init() {
-        self.favoritedPodcasts = store.fetch()
-    }
-    
-    func add(_ podcast: Podcast) {
-        favoritedPodcasts.append(podcast)
-        store.save(favoritedPodcasts) // 呼叫 store 來儲存
-    }
-    
-    func remove(_ podcast: Podcast) {
-        favoritedPodcasts.removeAll(where: { $0 == podcast })
-        store.save(favoritedPodcasts) // 呼叫 store 來儲存
-    }
-}
-
 struct FavoritesDataStore {
     
     private let userDefaults = UserDefaults.standard
